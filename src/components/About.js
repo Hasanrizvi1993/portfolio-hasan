ad2cd0d9afae212c9bde6f33faf0e5a15871dfa6
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-05-04 19:51:49 -0700

    more changes

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,15 @@
 // src/components/About.js
 import React from "react";
-import '../App.css'
+import "../App.css";
 // import myImage from "../../public/assets/images/IMG_2032";
 import myImage from "../../src/hasan.jpeg";
-import githubpic from "../../src/Octocat.jpg"
+import githubpic from "../../src/Octocat.jpg";
+import linkedin from "../../src/linkedin-logo-0.png";
 
 export default function About() {
 	return (
 		<section id='about'>
-			<div className="about-me-text">
+			<div className='about-me-text'>
 				<div>
 					<h1>
 						My name is Hasan Rizvi
@@ -16,15 +17,31 @@ export default function About() {
 						Get to know me
 					</h1>
 					<p>I love to build amazing apps. Look at my amazing projects!</p>
+					<div className='personalImage'>
+						<img src={myImage} alt='myimage' id='personalImage' />
+					</div>
+					<hr></hr>
+					<br></br>
+					<h2>Technologies</h2>
+					<p>Javascript, Python, HTML/ CSS, SQL</p>
+
+					<p>React, MERN Stack, Bootstrap</p>
+
+					<p>Node, Express, Django</p>
+
+					<p>NOSQL Databases, MongoDB, JSON, API usage</p>
+					<br></br><br></br>
+					<hr></hr>
 					<h3>PHONE</h3>
 					<p>(951)518-4719</p>
 					<h3>EMAIL</h3>
 					<p>hasanrizvi1993@gmail.com</p>
-					<a href="https://github.com/Hasanrizvi1993/"><img src={githubpic} alt='githubpic' id="githubpic"/></a>
-					<a href="https:www.linkedin.com/in/rizvi-hasan" title="Image from freeiconspng.com"><img src="https://www.freeiconspng.com/uploads/linkedin-logo-0.png" alt="Get Linkedin Logo Png Pictures" id="linkedinpic"/></a>
-				</div>
-				<div className="personalImage">
-					<img src={myImage} alt='myimage'id="personalImage"/>
+					<a href='https://github.com/Hasanrizvi1993/'>
+						<img src={githubpic} alt='githubpic' id='githubpic' />
+					</a>
+					<a href='https:www.linkedin.com/in/rizvi-hasan'>
+						<img src={linkedin} alt='linkedinpic' id='linkedinpic' />
+					</a>
 				</div>
 			</div>
 		</section>
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,6 @@
 import React from "react";
 
 export default function Contact() {
-	const [name, setName] = React.useState("");
-	const [email, setEmail] = React.useState("");
-	const [message, setMessage] = React.useState("");
-
-	function encode(data) {
-		return Object.keys(data)
-			.map(
-				(key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]),
-			)
-			.join("&");
-	}
-
-	function handleSubmit(e) {
-		e.preventDefault();
-		fetch("/", {
-			method: "POST",
-			headers: { "Content-Type": "application/x-www-form-urlencoded" },
-			body: encode({ "form-name": "contact", name, email, message }),
-		})
-			.then(() => alert("Message sent!"))
-			.catch((error) => alert(error));
-	}
-
 	return (
 		<section id='contact'>
 			<div>
@@ -50,42 +27,11 @@ export default function Contact() {
 							<p>hasanrizvi1993@gmail.com</p>
 							<h2>PHONE</h2>
 							<p>(951)518-4719</p>
+							<br></br>
+							<br></br>
 						</div>
 					</div>
 				</div>
-				<form netlify name='contact' onSubmit={handleSubmit}>
-					<h2>Hire Me</h2>
-					<p>
-						Need help of an amazing developer?
-					</p>
-					<div>
-						<label htmlFor='name'>Name</label>
-						<input
-							type='text'
-							id='name'
-							name='name'
-							onChange={(e) => setName(e.target.value)}
-						/>
-					</div>
-					<div className='relative mb-4'>
-						<label htmlFor='email'>Email</label>
-						<input
-							type='email'
-							id='email'
-							name='email'
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</div>
-					<div className='relative mb-4'>
-						<label htmlFor='message'>Message</label>
-						<textarea
-							id='message'
-							name='message'
-							onChange={(e) => setMessage(e.target.value)}
-						/>
-					</div>
-					<button type='submit'>Submit</button>
-				</form>
 			</div>
 		</section>
 	);
