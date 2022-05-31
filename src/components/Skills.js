449301b0281f72a19aba7fab7bcbbab0fd35900f
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-05-30 19:03:48 -0700

    s

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,46 +1,48 @@
 import React from 'react';
-
+import "../Skills.css";
 
 export default function Skills() {
     return(
         <section id="skills">
             <div id='skills-title' className='skills-container'>
                 <h2 className="skills-title">Tools In My Belt</h2>
+                <br></br>
+                <br></br>
             <div className='stacks'>
                 <div className='stack-item'>
-                    <p>Python</p>
+                    <p className='technology'>Python</p>
                     <a href="https://cdnlogo.com/logo/python_358.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/p/3/python.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>Javascript</p>
+                    <p className='technology'>Javascript</p>
                     <a href="https://cdnlogo.com/logo/javascript_36689.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/j/33/javascript.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>HTML</p>
+                    <p className='technology'>HTML</p>
                     <a href="https://cdnlogo.com/logo/html-5_8516.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/h/90/html-5.svg" alt=""></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>CSS</p>
+                    <p className='technology'>CSS</p>
                     <a href="https://cdnlogo.com/logo/css_36679.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/c/18/css.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>React</p>
+                    <p className='technology'>React</p>
                     <a href="https://cdnlogo.com/logo/react_22568.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/r/63/react.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>MongoDB</p>
+                    <p className='technology'>MongoDB</p>
                     <a href="https://cdnlogo.com/logo/mongodb-icon_41043.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/m/30/mongodb-icon.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>PostgreSQL</p>
+                    <p className='technology'>PostgreSQL</p>
                     <a href="https://cdnlogo.com/logo/postgresql_39744.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/p/93/postgresql.svg" alt=''></img></a>
                 </div>
-                <div  className='stack-item'>
-                    <p>Django</p>
+                <div className='stack-item'>
+                    <p className='technology'>Django</p>
                     <a href="https://cdnlogo.com/logo/django_40656.html"><img class="icon" src="https://cdn.cdnlogo.com/logos/d/3/django.svg" alt=''></img></a>
                 </div>
                 <div className='stack-item'>
-                    <p>Express</p>
+                    <p className='technology'>Express</p>
                     <img class="icon" src="https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg"alt=""></img>
                 </div>
             </div>   
