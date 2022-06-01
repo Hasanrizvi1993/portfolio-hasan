eec4f2af44987f10f113350a42b81bbc56ca401e
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2022-05-31 18:14:51 -0700

    made changes to make it mobile responsive

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,6 @@ import React from "react";
 import "../App.css";
 // import myImage from "../../public/assets/images/IMG_2032";
 import myImage from "../../src/hasan.jpeg";
-import githubpic from "../../src/Octocat.jpg";
-import linkedin from "../../src/linkedin-logo-0.png";
 
 export default function About() {
 	return (
@@ -22,12 +20,6 @@ export default function About() {
 					Currently interning at Beige Corporation as a Software Engineer building mutiple web application based on customer segments.
 					I am motivated by the story that each of our creations can tell, whether that be writing an inspiration-struck short story or building a carefully cultivated website for any business needs.</p>
 					
-					<a href='https://github.com/Hasanrizvi1993/'>
-						<img src={githubpic} alt='githubpic' id='githubpic' />
-					</a>
-					<a href='https://www.linkedin.com/in/rizvi-hasan'>
-						<img src={linkedin} alt='linkedinpic' id='linkedinpic' />
-					</a>
 			</div>
 			<div className='personalImage'>
 						<img src={myImage} alt='myimage' id='personalImage' />
