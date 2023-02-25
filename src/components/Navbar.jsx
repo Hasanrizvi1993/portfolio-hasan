0b07a86f783a4c12314b240ee74321eb928ede92
hasanrizvi1993 <hasanrizvi1993@gmail.com>
2023-02-25 15:32:17 -0800

    adjusted the navbar by removing about and contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav,  Navbar } from 'react-bootstrap';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar } from "react-bootstrap";
 
 const Navigation = () => {
 	return (
-		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-		<Navbar.Brand href="#about">About</Navbar.Brand>
-		<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-		<Navbar.Collapse id="responsive-navbar-nav">
-			<Nav>
-				<Nav.Link href="#contact">Contact</Nav.Link>
-				<Nav.Link eventKey={2} href="#projects">
-					Projects
-				</Nav.Link>
-			</Nav>
-		</Navbar.Collapse>
-	</Navbar>
+		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
+			<Navbar.Collapse id='responsive-navbar-nav'>
+				<Nav>
+					<Nav.Link eventKey={2} href='#projects'>
+						Projects
+					</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
 	);
 };
 
